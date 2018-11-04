30ac2906946a823d689699fa4114f47454de9d97
Tobik1610 <39301495+Tobik1610@users.noreply.github.com>
2018-11-04 14:21:42 +0100

    Kommentare

--- src/Main.java
+++ src/Main.java
@@ -17,11 +17,11 @@ public class Main {
 		try {
 			img = ImageIO.read(new File("src/sonnenuntergang.jpg"));
 			
-//			color = colorReconizer.AnalyzeImage(img, 10, 10);//Werte nicht größer als Auflösung
-//			new ImageFrame(img.getWidth(), img.getHeight(), color, true);
+			color = colorReconizer.AnalyzeImage(img, 10, 10);//Werte nicht größer als Auflösung
+			new ImageFrame(img.getWidth(), img.getHeight(), color, true);
 			
-			color = colorReconizer.AnalyzeImage(img, img.getWidth(), img.getHeight());//Werte nicht größer als Auflösung
-			new ImageFrame(img.getWidth(), img.getHeight(), color, false);
+//			color = colorReconizer.AnalyzeImage(img, img.getWidth(), img.getHeight());//Werte nicht größer als Auflösung
+//			new ImageFrame(img.getWidth(), img.getHeight(), color, false);
 
 //			for (int i = 0; i < color.length; i++) {
 //				for (int j = 0; j < color[i].length; j++) {
