37029a71f71a67d1033c96863b6f5c2c2824e263
Tobik1610 <39301495+Tobik1610@users.noreply.github.com>
2018-11-04 13:40:42 +0100

    Border

--- src/ImageFrame.java
+++ src/ImageFrame.java
@@ -4,37 +4,38 @@ import java.awt.Graphics;
 
 import javax.swing.JFrame;
 
-public class ImageFrame extends JFrame{
-	
+public class ImageFrame extends JFrame {
+
 	private Color[][] colors;
 	private int width, height;
 	private boolean borders;
 
-	public ImageFrame(int width, int height, Color[][] colors) {
+	public ImageFrame(int width, int height, Color[][] colors, boolean borders) {
 		this.colors = colors;
 		this.width = width;
 		this.height = height;
-		this.borders = true;
-		
+		this.borders = borders;
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setPreferredSize(new Dimension(width, height));
+		setPreferredSize(new Dimension(width + 17, height + 41));
 		setTitle("Farberkennung");
 		setVisible(true);
 		pack();
 		setLocationRelativeTo(null);
 	}
-	
+
 	public void paint(Graphics g) {
-		int x = width/colors.length;
-		int y = height/colors[0].length;
+		int x = width / colors.length;
+		int y = height / colors[0].length;
 		for (int i = 0; i < colors.length; i++) {
 			for (int j = 0; j < colors[i].length; j++) {
 				g.setColor(colors[i][j]);
-				g.fillRect(i * x, j * y, x, y);
+				g.fillRect(i * x + 8, j * y + 33, x, y);
+				if (borders) {
+					g.setColor(Color.BLACK);
+					g.drawRect(i * x + 8, j * y + 33, x, y);
+				}
 			}
-		}
-		if(borders) {
-			
 		}
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -16,16 +16,18 @@ public class Main {
 		Color[][] color;
 		try {
 			img = ImageIO.read(new File("src/sonnenuntergang.jpg"));
-			color = colorReconizer.AnalyzeImage(img, 960, 960);//Werte nicht größer als Auflösung
+			
+//			color = colorReconizer.AnalyzeImage(img, 10, 10);//Werte nicht größer als Auflösung
+//			new ImageFrame(img.getWidth(), img.getHeight(), color, true);
+			
+			color = colorReconizer.AnalyzeImage(img, img.getWidth(), img.getHeight());//Werte nicht größer als Auflösung
+			new ImageFrame(img.getWidth(), img.getHeight(), color, false);
 
 //			for (int i = 0; i < color.length; i++) {
 //				for (int j = 0; j < color[i].length; j++) {
 //					System.out.println(i + "/" + j + " " + color[i][j][1] + " " + color[i][j][2] + " " + color[i][j][3]);
 //				}
 //			}
-
-			//Frame
-			new ImageFrame(img.getWidth(), img.getHeight(), color);
 
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
